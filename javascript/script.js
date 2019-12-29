5b98997e4d9ead021af2b2909354038e8d72148e
Jacob Dolph <jacob.dolph702@gmail.com>
2019-12-29 10:45:06 -0700

    added icons to the 5 day forecast

--- javascript/script.js
+++ javascript/script.js
@@ -38,12 +38,22 @@ $(".search-button").on("click", function (event) {
             var weatherData = data;
             var currentWeatherIcon = data.weather[0].icon;
             console.log(currentWeatherIcon)
+            var currentWeatherIconEl = "http://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png";
+            console.log(currentWeatherIconEl);
+            var weathericon = $("<img/>", {
+                id: "weather-icon",
+                src: currentWeatherIconEl,
+                width: 75
+
+            });
+            console.log(weathericon)
             var weatherCard = $("<div>").addClass("card weather-card current-day-weather rounded-lg").attr("style", "width: 18rem");
             var cityDateEl = $("<h5>").addClass("card-title").text(weatherData.name + " " + "(" + date + ")");
-            var tempEl = $("<p>").addClass("card-text").text("Temperature: " + weatherData.main.temp + "F");
-            var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + weatherData.main.humidity);
-            var windspeedEl = $("<p>").addClass("card-text").text("Windspeed: " + weatherData.wind.speed);
+            var tempEl = $("<p>").addClass("card-text").text("Temperature: " + weatherData.main.temp + " F");
+            var humidityEl = $("<p>").addClass("card-text").text("Humidity: " + weatherData.main.humidity + " %");
+            var windspeedEl = $("<p>").addClass("card-text").text("Windspeed: " + weatherData.wind.speed + " mph");
             $(weatherCard).append(cityDateEl);
+            $(weatherCard).append(weathericon)
             $(weatherCard).append(tempEl);
             $(weatherCard).append(humidityEl);
             $(weatherCard).append(windspeedEl);
@@ -64,56 +74,95 @@ $(".search-button").on("click", function (event) {
             method: "GET"
         }).then(function (fiveData) {
             var fiveDayList = fiveData.list;
-            console.log(fiveDayList)
+
             //    each day is going to get its own var
-            var dayOne = fiveDayList[4]
+            var dayOne = fiveDayList[7]
+            console.log(dayOne)
             var dayOneIcon = dayOne.weather[0].icon;
-            console.log(dayOneIcon)
+            var dayOneWeatherIcon = "http://openweathermap.org/img/wn/" + dayOneIcon + ".png";
+            dayOneIconEl = $("<img/>", {
+                id: "weather-icon",
+                src: dayOneWeatherIcon,
+                width: 50
+            })
             var dayOneCard = $("<div>").addClass("card weather-card col bg-info text-white mr-md-4").attr("style", "width: 18rem");
             var dayOneDate = $("<h5>").addClass("card-title").text(moment().add(1, 'days').format('L'));
             var dayOneTemp = $("<p>").addClass("card-text").text("Temperature: " + dayOne.main.temp + "F");
             var dayOneHum = $("<p>").addClass("card-text").text("Humidity: " + dayOne.main.humidity);
             $(dayOneCard).append(dayOneDate);
+            $(dayOneCard).append(dayOneIconEl)
             $(dayOneCard).append(dayOneTemp);
             $(dayOneCard).append(dayOneHum);
             $("#five-day-forecast").append(dayOneCard);
             // ------------------------
-            var dayTwo = fiveDayList[12]
+            var dayTwo = fiveDayList[15]
+            var dayTwoIcon = dayTwo.weather[0].icon;
+            var dayTwoWeatherIcon = "http://openweathermap.org/img/wn/" + dayTwoIcon + ".png";
+            dayTwoIconEl = $("<img/>", {
+                id: "weather-icon",
+                src: dayTwoWeatherIcon,
+                width: 50
+            })
             var dayTwoCard = $("<div>").addClass("card weather-card col bg-info text-white mr-md-4").attr("style", "width: 18rem");
             var dayTwoDate = $("<h5>").addClass("card-title").text(moment().add(2, 'days').format('L'));
             var dayTwoTemp = $("<p>").addClass("card-text").text("Temperature: " + dayTwo.main.temp + "F");
             var dayTwoHum = $("<p>").addClass("card-text").text("Humidity: " + dayTwo.main.humidity);
             $(dayTwoCard).append(dayTwoDate);
+            $(dayTwoCard).append(dayTwoIconEl);
             $(dayTwoCard).append(dayTwoTemp);
             $(dayTwoCard).append(dayTwoHum);
             $("#five-day-forecast").append(dayTwoCard);
             // ------------------------
-            var dayThree = fiveDayList[20]
+            var dayThree = fiveDayList[23]
+            var dayThreeIcon = dayThree.weather[0].icon;
+            var dayThreeWeatherIcon = "http://openweathermap.org/img/wn/" + dayThreeIcon + ".png";
+            dayThreeIconEl = $("<img/>", {
+                id: "weather-icon",
+                src: dayThreeWeatherIcon,
+                width: 50
+            })
             var dayThreeCard = $("<div>").addClass("card weather-card col bg-info text-white mr-md-4").attr("style", "width: 18rem");
             var dayThreeDate = $("<h5>").addClass("card-title").text(moment().add(3, 'days').format('L'));
             var dayThreeTemp = $("<p>").addClass("card-text").text("Temperature: " + dayThree.main.temp + "F");
             var dayThreeHum = $("<p>").addClass("card-text").text("Humidity: " + dayThree.main.humidity);
             $(dayThreeCard).append(dayThreeDate);
+            $(dayThreeCard).append(dayThreeIconEl);
             $(dayThreeCard).append(dayThreeTemp);
             $(dayThreeCard).append(dayThreeHum);
             $("#five-day-forecast").append(dayThreeCard);
             // ------------------------
-            var dayFour = fiveDayList[28]
+            var dayFour = fiveDayList[31]
+            var dayFourIcon = dayFour.weather[0].icon;
+            var dayFourWeatherIcon = "http://openweathermap.org/img/wn/" + dayFourIcon + ".png";
+            dayFourIconEl = $("<img/>", {
+                id: "weather-icon",
+                src: dayFourWeatherIcon,
+                width: 50
+            })
             var dayFourCard = $("<div>").addClass("card weather-card col bg-info text-white mr-md-4").attr("style", "width: 18rem");
             var dayFourDate = $("<h5>").addClass("card-title").text(moment().add(4, 'days').format('L'));
             var dayFourTemp = $("<p>").addClass("card-text").text("Temperature: " + dayFour.main.temp + "F");
             var dayFourHum = $("<p>").addClass("card-text").text("Humidity: " + dayFour.main.humidity);
             $(dayFourCard).append(dayFourDate);
+            $(dayFourCard).append(dayFourIconEl);
             $(dayFourCard).append(dayFourTemp);
             $(dayFourCard).append(dayFourHum);
             $("#five-day-forecast").append(dayFourCard);
             // ------------------------
-            var dayFive = fiveDayList[36]
+            var dayFive = fiveDayList[39]
+            var dayFiveIcon = dayFive.weather[0].icon;
+            var dayFiveWeatherIcon = "http://openweathermap.org/img/wn/" + dayFiveIcon + ".png";
+            dayFiveIconEl = $("<img/>", {
+                id: "weather-icon",
+                src: dayFiveWeatherIcon,
+                width: 50
+            })
             var dayFiveCard = $("<div>").addClass("card weather-card col bg-info text-white").attr("style", "width: 18rem");
             var dayFiveDate = $("<h5>").addClass("card-title").text(moment().add(5, 'days').format('L'));
             var dayFiveTemp = $("<p>").addClass("card-text").text("Temperature: " + dayFive.main.temp + "F");
             var dayFiveHum = $("<p>").addClass("card-text").text("Humidity: " + dayFive.main.humidity);
             $(dayFiveCard).append(dayFiveDate);
+            $(dayFiveCard).append(dayFiveIconEl);
             $(dayFiveCard).append(dayFiveTemp);
             $(dayFiveCard).append(dayFiveHum);
             $("#five-day-forecast").append(dayFiveCard);
